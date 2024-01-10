c6fd43736b79005d1874ea278bc2d1142e72ac4d
zubayertahmid <95120572+zubayertahmid@users.noreply.github.com>
2024-01-10 18:35:34 +0600

    Shei update
    Onek kisu kore felsi

--- TherapyPortal.py
+++ TherapyPortal.py
@@ -7,14 +7,11 @@
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
-
-
-
-
-from PyQt5.QtGui import *
+import sys
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-import sys
+from PyQt5.QtGui import QFont
 
 class TherapyPortal(object):
     def TherapyPortalUi(self, Form):
